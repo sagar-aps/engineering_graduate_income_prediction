985ba7fe5907270f92ff4015325434590a31a3c2
sagetorix <sagetorix@pop-os.localdomain>
2021-05-25 23:32:30 +0200

    Created function to help clean data and organized engineering disciplines into 6 fields

--- Main.R
+++ Main.R
@@ -36,7 +36,20 @@ data %>% group_by(X10board) %>%
   filter(X10board != "cbse")
 
 #central, cbse, dav , "delhi public school" , "jawahar navodaya vidyalaya ","all india board " , "cbsc"=> cbse
-#certificate, icse, cisce, isc, anglo => icse
+#"certificate", "icse", "cisce", "isc", "anglo" => icse
+
+cbse_alt_names <- c("central","cbse","delhi public school", "jawahar navodaya vidyalaya ","all india board " ,"cbsc")
+icse_alt_names <- c("certificate", "icse", "cisce", "isc", "anglo")
+
+data %>% mutate(
+  X10board=ifelse(str_detect(X10board,paste(cbse_alt_names,collapse = "|")),"cbse",X10board),
+  X10board=ifelse(str_detect(X10board,paste(icse_alt_names,collapse = "|")),"icse",X10board),
+  X12board=ifelse(str_detect(X12board,paste(cbse_alt_names,collapse = "|")),"cbse",X12board),
+  X12board=ifelse(str_detect(X12board,paste(icse_alt_names,collapse = "|")),"icse",X12board),
+  X10board=ifelse(str_detect(X10board,paste(c("cbse","icse"),collapse = "|")),X10board,"state board"),
+  X12board=ifelse(str_detect(X12board,paste(c("cbse","icse"),collapse = "|")),X12board,"state board")) %>%
+  group_by(X10board,X12board) %>%
+  summarise(n=n())
 
 
 
@@ -45,9 +58,63 @@ data %>% group_by(X10board) %>%
 
 
 
+temp<-data %>%
+  group_by(Specialization) %>%
+  summarise(n=n()) %>%
+  arrange(n) 
+#%>%
+#  ggplot(aes(n))+
+ # geom_histogram(bins = 10)
+temp
+
+replace_by <- function(target, pattern, replaceby = deparse(substitute(pattern)), ifnotfound=target) {
+  ifelse(str_detect(target, paste(pattern,collapse = "|")),replaceby,ifnotfound)
+}
+
+deparse(substitute(mechanical))
+
+mechanical <- c("industrial & management engineering","mechanical & production engineering",
+                "industrial engineering","automobile/automotive engineering","mechanical and automation",
+                "industrial & production engineering","mechanical engineering")
+computer <-c("computer and communication engineering" ,"computer networking", 
+             "computer science and technology","computer engineering",
+              "computer science & engineering")
+eintc <-c("electronics & instrumentation","control and instrumentation engineering","instrumentation engineering",
+         "mechatronics","applied electronics and instrumentation","electronics","embedded systems technology" ,
+         "electronics and computer engineering","telecommunication engineering","electronics engineering",
+         "instrumentation and control engineering","electronics & instrumentation eng" ,"electronics and communication engineering",
+         "electronics & telecommunications")
+electrical <- c("electrical and power engineering","electrical engineering","electrical")
+IT <- c("information & communication technology", "information science",	
+                            "information science engineering", "computer application",
+                            "information technology")
+other <- c("ceramic engineering","biomedical engineering" ,"metallurgical engineering","aeronautical engineering","chemical engineering","other","biotechnology","electronics and electrical engineering")
 
 
 
+
+temp2<-data %>% mutate(
+  spec2 = Specialization,
+  spec2 = replace_by(spec2,mechanical),
+  spec2 = replace_by(spec2,computer),
+  spec2 = replace_by(spec2,eintc),
+  spec2 = replace_by(spec2,electrical),
+  spec2 = replace_by(spec2,IT),
+  spec2 = replace_by(spec2,other))%>% 
+  select(Specialization,spec2) %>%
+  group_by(spec2,Specialization)%>%
+  summarise(n=n()) %>%
+  arrange(Specialization)
+
+
+
+# 
+# 
+# 
+# 
+ 
+# 
+# civil engineering
 
 
 
